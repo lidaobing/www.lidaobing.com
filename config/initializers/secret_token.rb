1b3e957d908bf87a9491620cc8319e26f0adcaa7
LI Daobing <lidaobing@gmail.com>
2011-08-14 15:41:02 +0800

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lidaobing::Application.config.secret_token = '0b2d8f1eb4ac4818779511775653728a93609ff883a3f42d25ffa49b98e636a34b5fdfc697e16d6f1513eaaf5eb3ae0ccc7402c2c2136127dd807089f67c8245'
